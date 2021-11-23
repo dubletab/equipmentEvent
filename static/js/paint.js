87d061b872b0689a6e88d2733a3e63baa0ac9c05
dubletab <gnevyshevdv@gmail.com>
2021-11-23 22:02:33 +0300

    demo commit

---
+++ static/js/paint.js
@@ -0,0 +1,79 @@
+"use strict";
+
+let mapTimer = new Map;
+
+
+fetch('../static/json/message.js')
+    .then(response => response.json())
+    .then(json => {
+            for(let event of json['data']){
+                
+                let blockEvent = document.getElementById(event.id);
+                
+                if(event.pressed) {
+                    blockEvent.classList.add('active');
+                    blockEvent.lastElementChild.classList.add('activeTimer');
+                    let timer = moment(event.latest_start_time, "DD.MM.YYYY HH:mm:ss");
+                    mapTimer.set(event.id, timer);
+                }
+                else{
+                    blockEvent.classList.remove('active');
+                    blockEvent.lastElementChild.classList.remove('activeTimer');
+                    let timer = moment(event.latest_start_time, "DD.MM.YYYY HH:mm:ss");
+                    mapTimer.set(event.id, timer);
+                }
+
+            }
+        
+        console.log(json)});
+
+document.addEventListener('click', (event) =>{
+
+    let elem = event.target.closest('.btn');
+                if(!elem) return;
+                elem.classList.toggle('active');
+                mapTimer.set(elem.id, moment());
+                elem.lastElementChild.classList.toggle('activeTimer');
+});
+
+
+
+    setInterval(() => {
+        let clock = document.querySelector('.timer');
+        let clockDate = document.querySelector('.timerDate');
+        try {
+            let dateNow = new Date();
+            clock.innerHTML = getDoubleCharacters(dateNow.getHours()) 
+            + ':' + getDoubleCharacters(dateNow.getMinutes()) 
+            + ':' + getDoubleCharacters(dateNow.getSeconds());
+            clockDate.innerHTML = getDoubleCharacters(dateNow.getDate()) 
+            + '.' + getDoubleCharacters(dateNow.getMonth() + 1) 
+            + '.' + getDoubleCharacters(dateNow.getFullYear());
+        } catch(error){
+            null;
+        }
+    }, 1000);
+
+
+
+setInterval(() => {
+    mapTimer.forEach((value, key) => {
+        let now = moment();
+        let d = (now.diff(value, 'days'));
+        let h = (now.diff(value, 'hours')%24);
+        let m = (now.diff(value, 'minutes')%60);
+        let s = (now.diff(value, 'seconds')%60);
+        document.getElementById(key).lastElementChild.innerHTML = getDoubleCharacters(d) 
+        + 'д ' + getDoubleCharacters(h) 
+        + ":" + getDoubleCharacters(m)
+        + ":" + getDoubleCharacters(s);
+
+    });
+}, 1000);
+
+
+
+function getDoubleCharacters(mean) {
+    if(mean < 10) return '0' + mean;
+    else return mean;
+} 
---
+++ static/json/message.js
@@ -0,0 +1,120 @@
+{
+    "result": true,
+    "data": [
+        {
+            "id": 8,
+            "pressed": true,
+            "latest_start_time": "09.08.2021 16:25:31",
+            "name_event": "Событие 1"
+        },
+        {
+            "id": 18,
+            "pressed": false,
+            "latest_start_time": "16.07.2021 11:24:09",
+            "name_event": "Событие 6"
+        },
+        {
+            "id": 25,
+            "pressed": true,
+            "latest_start_time": "01.07.2021 11:24:23",
+            "name_event": "Событие 3"
+        },
+        {
+            "id": 20,
+            "pressed": true,
+            "latest_start_time": "28.09.2021 09:44:32",
+            "name_event": "Событие 8"
+        },
+        {
+            "id": 26,
+            "pressed": true,
+            "latest_start_time": "28.10.2021 13:23:29",
+            "name_event": "Событие 4"
+        },
+        {
+            "id": 17,
+            "pressed": true,
+            "latest_start_time": "09.11.2021 11:16:13",
+            "name_event": "Событие 5"
+        },
+        {
+            "id": 23,
+            "pressed": false,
+            "latest_start_time": "09.07.2021 11:37:55",
+            "name_event": "Событие 1"
+        },
+        {
+            "id": 12,
+            "pressed": true,
+            "latest_start_time": "09.07.2021 10:27:48",
+            "name_event": "Событие 5"
+        },
+        {
+            "id": 13,
+            "pressed": true,
+            "latest_start_time": "11.07.2021 23:16:14",
+            "name_event": "Событие 1"
+        },
+        {
+            "id": 9,
+            "pressed": true,
+            "latest_start_time": "09.08.2021 16:17:18",
+            "name_event": "Событие 2"
+        },
+        {
+            "id": 16,
+            "pressed": false,
+            "latest_start_time": "23.11.2021 08:10:48",
+            "name_event": "Событие 4"
+        },
+        {
+            "id": 15,
+            "pressed": false,
+            "latest_start_time": "23.11.2021 08:10:19",
+            "name_event": "Событие 3"
+        },
+        {
+            "id": 11,
+            "pressed": true,
+            "latest_start_time": "09.09.2021 16:17:24",
+            "name_event": "Событие 4"
+        },
+        {
+            "id": 24,
+            "pressed": true,
+            "latest_start_time": "23.11.2021 09:32:18",
+            "name_event": "Событие 2"
+        },
+        {
+            "id": 10,
+            "pressed": false,
+            "latest_start_time": "09.07.2021 08:23:51",
+            "name_event": "Событие 3"
+        },
+        {
+            "id": 19,
+            "pressed": false,
+            "latest_start_time": "09.07.2021 11:16:12",
+            "name_event": "Событие 7"
+        },
+        {
+            "id": 21,
+            "pressed": true,
+            "latest_start_time": "09.07.2021 14:13:20",
+            "name_event": "Событие 9"
+        },
+        {
+            "id": 22,
+            "pressed": false,
+            "latest_start_time": "09.07.2021 11:16:11",
+            "name_event": "Событие 10"
+        },
+        {
+            "id": 14,
+            "pressed": false,
+            "latest_start_time": "09.07.2021 14:13:05",
+            "name_event": "Событие 2"
+        }
+    ],
+    "canEdit": true
+}
